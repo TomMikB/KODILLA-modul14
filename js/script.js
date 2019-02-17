9caf1ed71b9a2ae2496a323fb71db75275e76e7d
Tomasz Biernacki <tmbiernacki@szczecin.home.pl>
2019-02-17 19:07:52 +0100

    Modul 14.3

--- js/script.js
+++ js/script.js
@@ -3,29 +3,46 @@
 (function(){
 
 	var templateList = document.getElementById('template').innerHTML;
-	Mustache.parse(templateList);
 	var cell = '';
-	for(var i = 0; i < cells.length; i++){
-		console.log(cells);
+	var carousel = document.querySelector('.carousel');
+	var restart = document.getElementById('restartBtn');
+	var progressBar = document.querySelector('.progress-bar');
+	var infos = document.getElementById('infos');
+
+	Mustache.parse(templateList);
+	for (var i = 0; i < cells.length; i++){
 		cell += Mustache.render(templateList, {number: cells[i].number, image: cells[i].image, title: cells[i].title, description: cells[i].description});
 	}
 	results.insertAdjacentHTML('beforeend', cell);
 
-	var carousel = document.querySelector('.carousel');
 	var flkty = new Flickity(carousel, {
 		pageDots: false,
 		hash: true
 	});
-	var restart = document.getElementById('restartBtn');
+	
 	addEventListener('click', function(event) {
 		if (!matchesSelector (event.target, '.my-btn')) {
 			return;
 		}
 		flkty.select(0);
 	});
-	var progressBar = document.querySelector('.progress-bar');
+	
 	flkty.on('scroll', function(progress) {
 		progress = Math.max(0, Math.min(1, progress));
 		progressBar.style.width = progress * 100 + '%';
 	});
+
+	window.initMap = function() {
+		var map = new google.maps.Map(document.getElementById('map'), {
+			zoom: 7,
+			center: cells[0].coords
+		});
+		for (var i=0; i < cells.length; i++){
+			var marker = new google.maps.Marker({
+				position: cells[i].coords,
+				map: map, 
+			});
+		}
+	};
+
 })();
